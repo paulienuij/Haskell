4ff44ca454b158a7072b51d5acda61f5cf6f41a9
Paulien Uijterwaal <paulien.uij@gmail.com>
2017-07-31 13:52:02 +0200

    solve problem 34

---
+++ 034.hs
@@ -0,0 +1,9 @@
+{-
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+-}
+import MyLibrary
+sumfac = [ x | x<-[3..], sum (map (factorial) (digits x)) == x]
+
+main = putStrLn $ show $  sum $ take 2 sumfac 
